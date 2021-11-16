9388a29a1585ab158c3ff7c422d0ded127999278
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-11-16 07:01:28 -0300

    added function to validate answer

--- src/components/QuestionBox.tsx
+++ src/components/QuestionBox.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, createElement, useContext, useState } from "react"
-import { Button, Chip, FormControlLabel, FormHelperText, Radio, RadioGroup, Stack, Typography } from "@material-ui/core"
+import { ChangeEvent, createElement, FormEvent, useContext, useState } from "react"
+import { Button, Chip, FormControl, FormControlLabel, FormHelperText, Radio, RadioGroup, Stack, Typography } from "@material-ui/core"
 
 import { QuestionsContext } from "../contexts/QuestionsContext"
 
@@ -23,8 +23,10 @@ export function QuestionBox({
   totalQuestions
 }: QuestionProps) {
   const { handleQuizSubmit } = useContext(QuestionsContext)
-  const [value, setValue] = useState('');
-  const [helperText, setHelperText] = useState('Choose wisely');
+  const [error, setError] = useState(false)
+  const [helperText, setHelperText] = useState(" ")
+  const [selectedAnswer, setSelectedAnswer] = useState("")
+  const hasAnswerSelected = selectedAnswer !== ""
 
   function renderQuestion(HTML: string) {
     return createElement("span", { dangerouslySetInnerHTML: { __html: HTML } })
@@ -35,8 +37,25 @@ export function QuestionBox({
   }
 
   function handleRadioChange(event: ChangeEvent<HTMLInputElement>) {
-    setValue((event.target as HTMLInputElement).value);
-    setHelperText('');
+    setSelectedAnswer((event.target as HTMLInputElement).value);
+    setHelperText(" ");
+  }
+
+  function handleAnswerSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    if(selectedAnswer === correct_answer) {
+      setHelperText("You got it!")
+      setError(false)
+    } else {
+      setHelperText("Sorry, wrong answer!")
+      setError(true)
+    }
+    setTimeout(() => {
+      handleQuizSubmit()
+      setError(false)
+      setHelperText(" ")
+      setSelectedAnswer("")
+    }, 2000)
   }
 
   return (
@@ -63,23 +82,39 @@ export function QuestionBox({
       <Typography variant="h5">
         { renderQuestion(question) }
       </Typography>
-      <RadioGroup
-        aria-label="quiz"
-        name="quiz"
-        value={ value }
-        onChange={ handleRadioChange }
-      >
-        { answers?.map((answer, index) => (
-          <FormControlLabel
-            key={ `answer${ index }` }
-            value={ answer }
-            control={ <Radio color="secondary" sx={ { color: theme.palette.secondary.main } } /> }
-            label={ renderAnswer(answer) }
-          />
-        )) }
-      </RadioGroup>
-      <FormHelperText>{ helperText }</FormHelperText>
-      <Button variant="contained" onClick={ handleQuizSubmit }>Confirm</Button>
+      <form onSubmit={ handleAnswerSubmit }>
+        <FormControl component="fieldset" error={ error } sx={ { width: "100%" } }>
+          <RadioGroup
+            aria-label="quiz"
+            name="quiz"
+            value={ selectedAnswer }
+            onChange={ handleRadioChange }
+          >
+            { answers?.map((answer, index) => (
+              <FormControlLabel
+                key={ `answer${ index }` }
+                value={ answer }
+                control={ <Radio color="secondary" sx={ { color: theme.palette.secondary.main } } /> }
+                label={ renderAnswer(answer) }
+              />
+            )) }
+          </RadioGroup>
+          <FormHelperText sx={ {
+            fontSize: '1rem',
+            color: theme.palette.success.main
+          } }>
+            { helperText }
+          </FormHelperText>
+          <Button
+            variant="contained"
+            type="submit"
+            disabled={ !hasAnswerSelected }
+            sx={ { mt: 2 } }
+          >
+            Confirm
+          </Button>
+        </FormControl>
+      </form>
     </Stack>
   )
 }
